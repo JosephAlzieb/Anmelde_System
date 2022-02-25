4ffc4b5a9455720a65bf672d747d6d66b3df88f2
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-25 01:24:53 +0100

    Termin is the aggregate root

---
+++ domain/src/main/java/termin/Termin.java
@@ -0,0 +1,36 @@
+package termin;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Termin {
+    private int id;
+    private LocalDate anmeldeFrist;
+    private LocalDateTime von;
+    private LocalDateTime bis;
+    private String titel ;
+    private Gruppe gruppe;
+
+    public Termin(int id, LocalDate anmeldeFrist, LocalDateTime von, LocalDateTime bis, String titel, Gruppe gruppe) {
+        this.id = id;
+        this.anmeldeFrist = anmeldeFrist;
+        this.von = von;
+        this.bis = bis;
+        this.titel = titel;
+        this.gruppe = gruppe;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Termin)) return false;
+        Termin termin = (Termin) o;
+        return id == termin.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
