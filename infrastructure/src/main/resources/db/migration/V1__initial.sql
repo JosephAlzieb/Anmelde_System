2ef8302d5c3489d6ae52667830b5aac356bb1c6d
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-25 17:29:56 +0100

    tables created

---
+++ infrastructure/src/main/resources/db/migration/V1__initial.sql
@@ -0,0 +1,51 @@
+
+create table if not exists termin
+(
+    Id int auto_increment,
+    anmeldefrist text not null,
+    von text not null,
+    bis text not null,
+    titel varchar(50) not null,
+    constraint termin_pk
+        primary key (Id)
+);
+
+
+
+create table if not exists gruppe
+(
+    Id int auto_increment,
+    constraint gruppe_pk
+        primary key (Id)
+);
+
+
+create table user
+(
+    gihub varchar(30) not null,
+    name varchar(30) not null,
+    matrikelnummer int not null,
+    gruppeId int not null,
+    constraint user_pk
+        primary key (gihub),
+    constraint user_gruppe_Id_fk
+        foreign key (gruppeId) references gruppe (Id)
+            on update cascade on delete cascade
+);
+
+
+
+create table belegung
+(
+    gruppeId int not null,
+    terminId int not null,
+    constraint belegung_pk
+        primary key (terminId),
+    constraint belegung_gruppe_Id_fk
+        foreign key (gruppeId) references gruppe (Id)
+            on update cascade on delete cascade,
+    constraint belegung_termin_Id_fk
+        foreign key (terminId) references termin (Id)
+            on update cascade on delete cascade
+);
+
