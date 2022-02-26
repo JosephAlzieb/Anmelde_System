39fd06c92e03750c7dc03b939f5b5c780d69883a
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-26 14:40:38 +0100

    findAll method override

--- infrastructure/src/main/java/de/infrastructure/infrastructure/db/repositoryDao/GruppeRepoDao.java
+++ infrastructure/src/main/java/de/infrastructure/infrastructure/db/repositoryDao/GruppeRepoDao.java
@@ -1,6 +1,11 @@
 package de.infrastructure.infrastructure.db.repositoryDao;
 
+import de.infrastructure.infrastructure.db.dto.GruppeDto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface GruppeRepoDao extends CrudRepository<GruppeRepoDao, Long> {
+import java.util.List;
+
+public interface GruppeRepoDao extends CrudRepository<GruppeDto, Long> {
+
+    List<GruppeDto> findAll();
 }
