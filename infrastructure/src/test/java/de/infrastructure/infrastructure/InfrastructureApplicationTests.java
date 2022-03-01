aa5b3c63015167709c40dff0bc231c3b9b12a76e
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-03-01 17:21:24 +0100

    comment contextLoads out

--- infrastructure/src/test/java/de/infrastructure/infrastructure/InfrastructureApplicationTests.java
+++ infrastructure/src/test/java/de/infrastructure/infrastructure/InfrastructureApplicationTests.java
@@ -1,13 +1,14 @@
-package de.infrastructure.infrastructure;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class InfrastructureApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package de.infrastructure.infrastructure;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.Profile;
+//
+//@SpringBootTest
+//class InfrastructureApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
