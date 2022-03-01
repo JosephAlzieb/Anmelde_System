342eedd845507775e54e9c248b1226e9399308d8
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-03-01 01:41:51 +0100

    add a set of users with a reference to the column in the database + gette and setter

--- infrastructure/src/main/java/de/infrastructure/infrastructure/db/dto/GruppeDto.java
+++ infrastructure/src/main/java/de/infrastructure/infrastructure/db/dto/GruppeDto.java
@@ -1,11 +1,36 @@
 package de.infrastructure.infrastructure.db.dto;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.Set;
+
 @Table("gruppe")
-public record GruppeDto(
+public class GruppeDto {
         @Id
-        Long id
-) {
+        Long id;
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        @MappedCollection(idColumn = "gruppeId")
+        private Set<UserDto> users;
+
+
+        public GruppeDto() {
+        }
+
+        public Set<UserDto> getUsers() {
+                return users;
+        }
+
+        public void setUsers(Set<UserDto> users) {
+                this.users = users;
+        }
 }
