81ccf9171c20e70521525cd631d99994416f1d55
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-03-01 18:01:49 +0100

    save gruppe not working

--- infrastructure/src/main/java/de/infrastructure/infrastructure/db/repositoryImpl/GruppeRepoImpl.java
+++ infrastructure/src/main/java/de/infrastructure/infrastructure/db/repositoryImpl/GruppeRepoImpl.java
@@ -1,12 +1,15 @@
 package de.infrastructure.infrastructure.db.repositoryImpl;
 
+import de.infrastructure.infrastructure.db.dto.GruppeDto;
+import de.infrastructure.infrastructure.db.dto.UserDto;
 import de.infrastructure.infrastructure.db.repositoryDao.GruppeRepoDao;
 import gruppe.Gruppe;
 import org.springframework.stereotype.Repository;
 import repositories.GruppeRepository;
 
 import java.util.List;
-
+import java.util.Set;
+import java.util.stream.Collectors;
 @Repository
 public class GruppeRepoImpl implements GruppeRepository {
 
@@ -25,17 +28,21 @@ public class GruppeRepoImpl implements GruppeRepository {
 
     @Override
     public void saveGruppe(Gruppe gruppe) {
-        GruppeDto dto = repoDao.findById(gruppe.getId()).get();
-        Set<UserDto> userDtos = gruppe.getUsers().stream().map(u->new UserDto(u.getGihubHandle(),u.getName(),u.getMatrikelNummer().getNr())).collect(Collectors.toSet());
-        dto.setUsers(userDtos);
-        System.out.println(dto);
-        System.out.println(userDtos);
+//        GruppeDto dto = repoDao.findById(gruppe.getId()).get();
+//        Set<UserDto> userDtos = gruppe.getUsers().stream().map(u->new UserDto(u.getGihubHandle(),u.getName(),u.getMatrikelNummer().getNr())).collect(Collectors.toSet());
+//        dto.setUsers(userDtos);
+//        System.out.println(dto);
+//        System.out.println(userDtos);
 //        repoDao.save(dto);
     }
 
     @Override
-    public void addUser(String gihubHandle, String name, String matrikelNummer) {
-
+    public void addUser(Long gruppeId,String gihubHandle, String name, String matrikelNummer) {
+//        Optional<GruppeDto> gruppeDto = repoDao.findById(gruppeId);
+//        if(gruppeDto.isPresent()){
+//            Gruppe gruppe = new Gruppe(gruppeDto.get().id()).addUser(gihubHandle,name,matrikelNummer);
+//            saveGruppe(gruppe);
+//        }
     }
 
     @Override
