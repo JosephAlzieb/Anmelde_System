bfa40644257bea5be77e97fd759914da04897d4f
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-03-01 01:40:33 +0100

    config for services

--- infrastructure/src/main/java/de/infrastructure/infrastructure/config/Configuration.java
+++ infrastructure/src/main/java/de/infrastructure/infrastructure/config/Configuration.java
@@ -2,7 +2,9 @@ package de.infrastructure.infrastructure.config;
 
 import de.infrastructure.infrastructure.db.repositoryImpl.TerminRepoImpl;
 import org.springframework.context.annotation.Bean;
+import repositories.GruppeRepository;
 import repositories.TerminRepository;
+import services.GruppenService;
 import services.TerminService;
 
 @org.springframework.context.annotation.Configuration
@@ -11,5 +13,10 @@ public class Configuration {
     @Bean
     TerminService terminService (TerminRepository repository){
         return new TerminService (repository);
+    }
+
+    @Bean
+    GruppenService gruppenService (GruppeRepository repository){
+        return new GruppenService (repository);
     }
 }
